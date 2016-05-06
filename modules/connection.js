33f56b80aa075c54e91bb140350e3af3fe13bd86
John Rachuy II <john.rachuy@gmail.com>
2016-05-05 21:01:11 -0500

    Cleaning up code

--- modules/connection.js
+++ modules/connection.js
@@ -1,17 +1,17 @@
 var connectionString = '';
 
 //local host connectionString
-//if(process.env.DATABASE_URL !== undefined) {
-//    connectionString = process.env.DATABASE_URL + 'ssl';
-//} else {
-//    connectionString = 'postgres://localhost:5432/lazy_days';
-//}
+if(process.env.DATABASE_URL !== undefined) {
+    connectionString = process.env.DATABASE_URL + 'ssl';
+} else {
+    connectionString = 'postgres://localhost:5432/lazy_days';
+}
 
 //Heroku connectionString
-if(process.env.DATABASE_URL != undefined) {
-    connectionString = process.env.DATABASE_URL + "?ssl=true";
-} else {
-    connectionString = 'postgres://tozsnlivczuaum:myvv9PpdKbsaNlGGpWNFc_JlOL@ec2-54-225-112-119.compute-1.amazonaws.com:5432/d5a81tjgl385eg?ssl=true';
-}
+//if(process.env.DATABASE_URL != undefined) {
+//    connectionString = process.env.DATABASE_URL + "?ssl=true";
+//} else {
+//    connectionString = 'postgres://tozsnlivczuaum:myvv9PpdKbsaNlGGpWNFc_JlOL@ec2-54-225-112-119.compute-1.amazonaws.com:5432/d5a81tjgl385eg?ssl=true';
+//}
 
 module.exports = connectionString;
